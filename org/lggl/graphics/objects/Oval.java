a1fc6afb93b6c232371eec5d89722adab66d0ca9
XIOTeam <39484230+XIOTeam@users.noreply.github.com>
2018-05-21 14:55:51 +0200

    Add files via upload

---
+++ org/lggl/graphics/objects/Oval.java
@@ -0,0 +1,44 @@
+package org.lggl.graphics.objects;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import org.lggl.graphics.Window;
+
+public class Oval extends GameObject {
+
+	private boolean filled;
+	
+	public boolean isFilled() {
+		return filled;
+	}
+
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+
+	public Oval() {
+		this(0, 0, 50, 50, Color.BLACK);
+	}
+
+	public Oval(int x, int y, int width, int height, Color c) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		color = c;
+		hitbox = new java.awt.Rectangle(x, y, width, height);
+	}
+	
+	@Override
+	public void paint(Graphics g, Window source) {
+		g.setColor(getColor());
+		if (!filled)
+			g.drawOval(x, y, width, height);
+		else
+			g.fillOval(x, y, width, height);
+	}
+
+	
+	
+}
